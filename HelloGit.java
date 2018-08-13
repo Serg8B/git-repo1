66d5490c8853456a9656d15a17e9c93087e537bc
Serg8B <Sergio198585@gmail.com>
2018-08-13 12:54:38 +0300

    Проставляем точки  с запятыми

--- HelloGit.java
+++ HelloGit.java
@@ -1,5 +1,5 @@
 public class HelloGit{
-Sistem.out.print("Hello Git");
-Sistem.out.println("dfdfdsgsgfgrgbrgb");
-Sistem/out.print("Тра та таТА");
+Sistem.out.print("Hello Git;");
+Sistem.out.println("dfdfdsgsgfgrgbrgb;");
+Sistem/out.print("Тра та таТА;");
 }
